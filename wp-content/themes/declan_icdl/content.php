ba5fb1f1d36e62ddd56eb85608f6c6d35d768f2f
Declan Munroe <declan.munroe@ics.ie>
2018-08-16 09:11:40 +0100

    Test images after redeploy build

--- wp-content/themes/declan_icdl/content.php
+++ wp-content/themes/declan_icdl/content.php
@@ -2,6 +2,7 @@
 	<h2 class="blog-post-title"><?php the_title(); ?></h2>
 	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
         <p>Testing local</p>
+        <p>Testing image upload. Does it stay after reploy deploying project</p>
 
  <?php the_content(); ?>
 
