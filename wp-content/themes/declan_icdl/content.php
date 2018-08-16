5a9e586a6401d96c1404ffd9548174b9ce02918d
Declan Munroe <declan.munroe@ics.ie>
2018-08-16 11:20:25 +0100

    Test aws S3 images

--- wp-content/themes/declan_icdl/content.php
+++ wp-content/themes/declan_icdl/content.php
@@ -3,6 +3,7 @@
 	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
         <p>Testing local</p>
         <p>Testing image upload. Does it stay after reploy deploying project</p>
+        <p>Testing s3 image after project re build through git</p>
 
  <?php the_content(); ?>
 
